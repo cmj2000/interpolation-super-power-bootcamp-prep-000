197cc4e1defcb2fc304920d7f7b3d8f9e0c75349
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-05-23 23:56:21 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,6 +4,6 @@
  
  def display_rainbow(colors)
    
-   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: " 
+   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: " 
    
  end
